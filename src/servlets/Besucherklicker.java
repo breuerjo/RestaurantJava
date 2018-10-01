206cc20f81ab737338fbbc416c16290625d8188d
Jonas <Jonas@10.94.112.177>
2018-10-01 17:49:34 +0200

    Besucherklicker Servlet 0,5h

--- src/servlets/Besucherklicker.java
+++ src/servlets/Besucherklicker.java
@@ -9,26 +9,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Servlet01
- */
-@WebServlet("/src/Servlet01")
-public class Servlet01 extends HttpServlet {
+@WebServlet("/Besucherklicker")
+public class Besucherklicker extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static int zaehler=0;
     
-    public Servlet01() {
+    public Besucherklicker() {
         super();
         // TODO Auto-generated constructor stub
     }
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		zaehler++;
 		PrintWriter out = response.getWriter();
-		String contextParameterName = "anfangswert01";
-		String contextParameterWert = this.getInitParameter(contextParameterName);
-		out.println("Der Context-Parameter " + contextParameterName + " hat den Wert " +
-		contextParameterWert + ".");
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//String contextParameterName = "anfangswert01";
+		//String contextParameterWert = this.getInitParameter(contextParameterName);
+		//out.println("Der Context-Parameter " + contextParameterName + " hat den Wert " +contextParameterWert + ".");
+		out.println("<p><b>Besucher: "+zaehler+"</b></p>");
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	
